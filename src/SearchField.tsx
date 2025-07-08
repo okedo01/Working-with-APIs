bab5b9c2f8bb87cc8c2a634dbd8eed9113b074f0
Okedo01 <eliahmwelangi01@gmail.com>
2025-07-08 17:48:53 +0300

    Card

--- src/SearchField.tsx
+++ src/SearchField.tsx
@@ -1,21 +1,29 @@
 import React, { useEffect, useState } from 'react'
-// import { Input } from './components/ui/input'
 import type { User } from './Type';
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 
 const SearchField: React.FC = () => {
-    const [ data, setData ] = useState<User[]>([]);
-    const [ loading, setLoading ] = useState(true);
-    const [ error, setError ] = useState(true);
-    
-    if(!loading) <div>Loading...</div>
-    
-    if(!error) <div>Error: { error }</div>
+    const [data, setData] = useState<User[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(true);
+
+    if (!loading) <div>Loading...</div>
+
+    if (!error) <div>Error: {error}</div>
 
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then(response => {
-                if(!response.ok) {
+                if (!response.ok) {
                     throw new Error("Failed to fetch");
                 }
                 return response.json();
@@ -30,18 +38,25 @@ const SearchField: React.FC = () => {
             })
     }, [])
 
-  return (
-    <div>
-        {/* <Input placeholder="Search..."/> */}
-            { data.map((item) => (
-                <li key={item.id}>
-                    <p>
-                        { item.name }
-                    </p>
-                </li>
+    return (
+        <div>
+            {data.map(value => (
+                <Card key={value.id}>
+                    <CardHeader>
+                        <CardTitle>{ value.name}</CardTitle>
+                        <CardDescription>{ value.website }</CardDescription>
+                        <CardAction>{ value.company.name }</CardAction>
+                    </CardHeader>
+                    <CardContent>
+                        <p>{ value.email }</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>{ value.address.city }</p>
+                    </CardFooter>
+                </Card>
             ))}
-    </div>
-  )
+        </div>
+    )
 }
 
 export default SearchField
