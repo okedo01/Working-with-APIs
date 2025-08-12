e328b94d1dbe9d8d99048a3d1a32a66142b1e892
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-12 11:00:38 +0300

    React Query

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
+import { useQuery } from '@tanstack/react-query'
 import './App.css'
-import SearchField from './SearchField'
 
 function App() {
-
+  const { data } = useQuery({
+    queryKey: ['todo'], queryFn: () => fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Failed to fetch resources");
+        }
+        return res.json();
+      })
+  })
   return (
-    <>
-      <SearchField />
-    </>
+    <div className="bg-gray-400">
+      {data.map((todo: any) => (
+        <div>
+          <h1>ID: {todo.id}</h1>
+          <h1>TITLE: {todo.title}</h1>
+          <h1>{todo.id}</h1>
+        </div>
+      ))}
+    </div>
   )
 }
 
