6190d5bccad17087c2c6bcf6ccf0b174a129d274
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-14 15:07:24 +0300

    Install lodash package with Typescript definitions

--- src/ReactQuery.tsx
+++ src/ReactQuery.tsx
@@ -4,26 +4,29 @@ import React from 'react'
 const ReactQuery: React.FC = () => {
     const { data, error, isLoading } = useQuery({
         queryKey: ["todos"],
-        queryFn: async() => {
+        queryFn: async () => {
             const res = await fetch("https://jsonplaceholder.typicode.com/todos")
-            if(!res.ok) throw new Error("Failed to fetch data");
-            return res.json(); 
+            if (!res.ok) throw new Error("Failed to fetch data");
+            return res.json();
         }
     })
+
+    if (!data) return <div>No data Found</div>
 
     if (error) return <div>There is an error</div>
 
     if (isLoading) return <div>Loading...</div>
 
-    if (!data) return <div>No data Found</div>
-
     return (
         <>
-            {data.map((todo: any) => (
-                <div key={todo.id}>
-                    <h1>{todo.name}</h1>
-                </div>
-            ))}
+            <div>
+                { data.map((todo: any) => (
+                    <div key={todo.id}>
+                        <h1>ID: { todo.id }</h1>
+                        <h3>TITLE: { todo.title }</h3>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
