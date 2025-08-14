e3f0e14623f1cc3a11d9e852445812eb29fc28c2
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-14 16:38:27 +0300

    Install lodash package with Typescript definitions

--- src/ReactQuery.tsx
+++ src/ReactQuery.tsx
@@ -1,4 +1,4 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import React from 'react'
 
 const ReactQuery: React.FC = () => {
@@ -11,24 +11,47 @@ const ReactQuery: React.FC = () => {
         }
     })
 
-    if (!data) return <div>No data Found</div>
+    const { mutate } = useMutation({
+        mutationFn: async (newPost: any) => {
+          const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "Application/json"
+            },
+            body: JSON.stringify(newPost)
+          })
+          if(!res.ok) throw new Error("Failed to fetch data");
+          
+          return res.json();
+    }
+    })
 
-    if (error) return <div>There is an error</div>
+if (!data) return <div>No data Found</div>
 
-    if (isLoading) return <div>Loading...</div>
+if (error) return <div>There is an error</div>
 
-    return (
-        <>
-            <div>
-                { data.map((todo: any) => (
-                    <div key={todo.id}>
-                        <h1>ID: { todo.id }</h1>
-                        <h3>TITLE: { todo.title }</h3>
-                    </div>
-                ))}
-            </div>
-        </>
-    )
+if (isLoading) return <div>Loading...</div>
+
+return (
+    <>
+        <button onClick={() => {
+            mutate({
+                "userId": 200,
+                "id": 300,
+                "title": "TITTLE",
+                "body": "BODY"
+            })
+        }}>Add Post</button>
+        <div>
+            {data.map((todo: any) => (
+                <div key={todo.id}>
+                    <h1>ID: {todo.id}</h1>
+                    <h3>TITLE: {todo.title}</h3>
+                </div>
+            ))}
+        </div>
+    </>
+)
 }
 
 export default ReactQuery
