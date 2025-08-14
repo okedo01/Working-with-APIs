fde2afe44b3e258564b258230b25142218d3e722
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-14 16:53:52 +0300

    useMutation

--- src/ReactQuery.tsx
+++ src/ReactQuery.tsx
@@ -13,45 +13,45 @@ const ReactQuery: React.FC = () => {
 
     const { mutate } = useMutation({
         mutationFn: async (newPost: any) => {
-          const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
-            method: "POST",
-            headers: {
-                "Content-Type": "Application/json"
-            },
-            body: JSON.stringify(newPost)
-          })
-          if(!res.ok) throw new Error("Failed to fetch data");
-          
-          return res.json();
-    }
+            const res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "Application/json"
+                },
+                body: JSON.stringify(newPost)
+            })
+            if (!res.ok) throw new Error("Failed to fetch data");
+
+            return res.json();
+        }
     })
 
-if (!data) return <div>No data Found</div>
+    if (!data) return <div>No data Found</div>
 
-if (error) return <div>There is an error</div>
+    if (error) return <div>There is an error</div>
 
-if (isLoading) return <div>Loading...</div>
+    if (isLoading) return <div>Loading...</div>
 
-return (
-    <>
-        <button onClick={() => {
-            mutate({
-                "userId": 200,
-                "id": 300,
-                "title": "TITTLE",
-                "body": "BODY"
-            })
-        }}>Add Post</button>
-        <div>
-            {data.map((todo: any) => (
-                <div key={todo.id}>
-                    <h1>ID: {todo.id}</h1>
-                    <h3>TITLE: {todo.title}</h3>
-                </div>
-            ))}
-        </div>
-    </>
-)
+    return (
+        <>
+            <button onClick={() => {
+                mutate({
+                    "userId": 100,
+                    "id": 200,
+                    "title": "TITLE",
+                    "body": "BODY"
+                })
+            }}>Add Post</button >
+            <div>
+                {data.map((todo: any) => (
+                    <div key={todo.id}>
+                        <h1>ID: {todo.id}</h1>
+                        <h3>TITLE: {todo.title}</h3>
+                    </div>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default ReactQuery
