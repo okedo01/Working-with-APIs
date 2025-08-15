5de10963ea72af52cf26592492f8e5ba8b97136d
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-15 15:49:58 +0300

    Crete post

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query'
-import React, { useState, type ChangeEvent } from 'react'
+import React, { useState } from 'react'
 import { Button } from './components/ui/button';
 import { Input } from './components/ui/input';
 
@@ -24,6 +24,10 @@ const CreatePost: React.FC = () => {
     queryFn: getTodos
   })
 
+  const filterTodos = todos?.filter((todo: any) => {
+    return todo.title.toLowerCase().includes(searchTerm.toLowerCase());
+  })
+
   if(error) return <div>Error: {error.message}</div>
 
   if(isLoading) return <div>Loading...</div>
@@ -36,7 +40,7 @@ const CreatePost: React.FC = () => {
       </form>
     
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {todos.map((todo: any) => (
+        {filterTodos.map((todo: any) => (
           <div key={todo.id} className="bg-gray-400 rounded-2xl p-2 m-3">
             <h2>ID: {todo.id}</h2>
             <h2>TITLE: {todo.title}</h2>
