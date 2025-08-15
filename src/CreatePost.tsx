0865b40d40cdf1ea957e7be36a39c582b9cf145a
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-15 12:26:28 +0300

    Crete post

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -1,37 +1,51 @@
 import { useQuery } from '@tanstack/react-query'
-import React, { useState } from 'react'
+import React, { useState, type ChangeEvent } from 'react'
+import { Button } from './components/ui/button';
+import { Input } from './components/ui/input';
 
 const CreatePost: React.FC = () => {
 
-  const [ searchTerm, setSearchTerm] = useState<string>("");
+  const [searchTerm, setSearchTerm] = useState<string>("");
 
-  const getTodos = async() => {
+  const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  }
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  }
+
+  const getTodos = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
 
-    if(!res.ok) throw new Error("Failed to fetch data");
+    if (!res.ok) throw new Error("Failed to fetch data");
 
     return res.json();
   }
 
-  const { data: todos } = useQuery({
+  const { data: todos, error, isLoading } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos
   })
 
+  if(error) return <div>Error: {error.message}</div>
+
+  if(isLoading) return <div>Loading...</div>
+
   return (
     <main>
-      <section>
-        <input type="text" />
-        <input type="button" />
-      </section>
+      <form onSubmit={handleSubmit}>
+        <Input type="text" onChange={handleSearchInput} />
+        <Button value="Submit"/>
+      </form>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      { todos.map((todo: any) => (
-        <div key={todo.id} className="bg-gray-400 rounded-2xl p-2 m-3">
-          <h2>ID: { todo.id }</h2>
-          <h2>TITLE: { todo.title }</h2>
-        </div>
-      ))}
-    </section>
+        {todos.map((todo: any) => (
+          <div key={todo.id} className="bg-gray-400 rounded-2xl p-2 m-3">
+            <h2>ID: {todo.id}</h2>
+            <h2>TITLE: {todo.title}</h2>
+          </div>
+        ))}
+      </section>
     </main>
   )
 }
