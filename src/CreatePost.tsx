393243185c5a39f5dcab5df5ed3860a9e26d468b
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-15 11:52:58 +0300

    Crete post

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import React, { useState } from 'react'
 
 const CreatePost: React.FC = () => {
+
+  const [ searchTerm, setSearchTerm] = useState<string>("");
 
   const getTodos = async() => {
     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
@@ -18,7 +20,10 @@ const CreatePost: React.FC = () => {
 
   return (
     <main>
-      
+      <section>
+        <input type="text" />
+        <input type="button" />
+      </section>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
       { todos.map((todo: any) => (
         <div key={todo.id} className="bg-gray-400 rounded-2xl p-2 m-3">
