67936a07d8f9467829c39e309adb3a584e4fa791
Okedo01 <eliahmwelangi01@gmail.com>
2025-08-15 16:15:09 +0300

    Crete post

--- src/CreatePost.tsx
+++ src/CreatePost.tsx
@@ -1,7 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import React, { useState } from 'react'
 import { Button } from './components/ui/button';
 import { Input } from './components/ui/input';
+
+type Posts = {
+  title: string,
+  userId: string,
+  Id: string,
+  body: string
+}
 
 const CreatePost: React.FC = () => {
 
@@ -12,9 +19,23 @@ const CreatePost: React.FC = () => {
   }
 
   const getTodos = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
     if (!res.ok) throw new Error("Failed to fetch data");
+
+    return res.json();
+  }
+
+  const fetchTodos = async (newPost: Posts) => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8"
+      },
+      body: JSON.stringify(newPost)
+    })
+
+    if (!res.ok) throw new Error("Failed to fetch Data");
 
     return res.json();
   }
@@ -24,13 +45,17 @@ const CreatePost: React.FC = () => {
     queryFn: getTodos
   })
 
+  const { } = useMutation({
+    mutationFn: fetchTodos
+  })
+
   const filterTodos = todos?.filter((todo: any) => {
     return todo.title.toLowerCase().includes(searchTerm.toLowerCase());
   })
 
-  if(error) return <div>Error: {error.message}</div>
+  if (error) return <div>Error: {error.message}</div>
 
-  if(isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading...</div>
 
   return (
     <main>
@@ -38,7 +63,7 @@ const CreatePost: React.FC = () => {
         <Input type="text" onChange={(e) => setSearchTerm(e.target.value)} />
         <Button type='submit' className="mt-4 cursor-pointer">Add Post</Button>
       </form>
-    
+
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {filterTodos.map((todo: any) => (
           <div key={todo.id} className="bg-gray-400 rounded-2xl p-2 m-3">
